2aebb5c5c7b5d010629c3db2450c1bf674fca31a
KevinFGR <kevinfelipe.gr@gmail.com>
2024-09-21 15:32:26 -0300

    feat: starting form for details page

--- lib/details/details_view.dart
+++ lib/details/details_view.dart
@@ -1,110 +1,263 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_app/details/details_functions.dart';
 
-class DetailsPage extends StatelessWidget{
+class DetailsPage extends StatefulWidget{
   const DetailsPage({super.key});
+  
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage>{
+  final  _form = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  String? selectedUf;
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
+        title: Center(
+          child: FractionallySizedBox(
+          widthFactor: 0.4, 
+          child: Image.asset(
+            'assets/logo.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+        ),
         elevation: 0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.all(27),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.lightBlue[900]!, const Color.fromARGB(255, 0, 16, 28)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FractionallySizedBox(
-              widthFactor: 0.7, 
-              child: Image.asset(
-                'assets/logo_login.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              "Bem Vindo ao SMART",
-              style: TextStyle(
-                color:Colors.white,
-              ),
-            ),
-            const SizedBox(height:65),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Email',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(10),
-              placeholder: "email@email.com",
-              placeholderStyle: TextStyle(color:Colors.white70, fontSize:14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(7),
-                ),
-              ),
-            ),
-            const SizedBox(height:8),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Senha',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(10),
-              placeholder: "********",
-              placeholderStyle: TextStyle(color:Colors.white70, fontSize:14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(7),
-                ),
-              ),
-            ),
-            const SizedBox(height:30),
-            SizedBox(
-              width: double.infinity,
-              child: CupertinoButton(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(30),
-                ),
-                color: Colors.lightBlue[800],
-                child: const Text(
-                  "Acessar",
-                  style:TextStyle(color: Colors.white70, fontSize: 14, fontWeight: FontWeight.w600)
+        child: SingleChildScrollView(
+          child: Form(
+            key: _form,
+            child: Column(
+              children: [
+                const SizedBox(height: 90),
+                const Text(
+                  "Cadastrar Cliente",
+                  style: TextStyle(
+                    color:Colors.black,
+                    fontSize:27,
+                    fontWeight: FontWeight.w500,
                   ),
-                onPressed: () {},
                 ),
+                const SizedBox(height:30),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Email',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                CupertinoTextField(
+                  padding: const EdgeInsets.all(10),
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  placeholder: "email@email.com",
+                  placeholderStyle: const TextStyle(color:Colors.black38, fontSize:14),
+                  style: const TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'CNPJ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const CupertinoTextField(
+                  keyboardType: TextInputType.number,
+                  padding: EdgeInsets.all(10),
+                  placeholder: "00.000.000/0000-00",
+                  placeholderStyle: TextStyle(color:Colors.black38, fontSize:14),
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Razão Social',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const CupertinoTextField(
+                  padding: EdgeInsets.all(10),
+                  placeholder: "DIGITE AQUI...",
+                  placeholderStyle: TextStyle(color:Colors.black38, fontSize:14),
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Nome Fantasia',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const CupertinoTextField(
+                  padding: EdgeInsets.all(10),
+                  placeholder: "DIGITE AQUI...",
+                  placeholderStyle: TextStyle(color:Colors.black38, fontSize:14),
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Nome Fantasia',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const CupertinoTextField(
+                  padding: EdgeInsets.all(10),
+                  placeholder: "DIGITE AQUI...",
+                  placeholderStyle: TextStyle(color:Colors.black38, fontSize:14),
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Nome Fantasia',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const CupertinoTextField(
+                  padding: EdgeInsets.all(10),
+                  placeholder: "DIGITE AQUI...",
+                  placeholderStyle: TextStyle(color:Colors.black38, fontSize:14),
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                ),
+                const SizedBox(height:8),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'UF',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                  ),
+                  child: DropdownButton<String>(
+                    value: selectedUf,
+                    hint: const Text("UF"),
+                    isExpanded: true,
+                    items: DetailsFunctions.ufs().map((String uf) {
+                      return DropdownMenuItem<String>(
+                        value: uf,
+                        child: Text(uf),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedUf = newValue;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height:30),
+                SizedBox(
+                  width: double.infinity,
+                  child: CupertinoButton(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                    color: Colors.lightBlue[800],
+                    child: const Text(
+                      "Salvar",
+                      style:TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w600)
+                      ),
+                    onPressed: () {
+                      print(emailController.text);
+                      print(selectedUf);
+                    },
+                    ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
