a7e0abea2c6b2c319c7d0d783bf54a3a9efacc88
KevinFGR <kevinfelipe.gr@gmail.com>
2024-10-05 23:39:26 -0300

    remove unecessary code on list page

--- lib/app/list/list_view.dart
+++ lib/app/list/list_view.dart
@@ -24,84 +24,9 @@ class ListPage extends StatelessWidget{
       body: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.all(27),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.lightBlue[900]!, const Color.fromARGB(255, 0, 16, 28)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
         child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FractionallySizedBox(
-              widthFactor: 0.7, 
-              child: Image.asset(
-                'assets/logo_login.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              "Bem Vindo á Página de listagem",
-              style: TextStyle(
-                color:Colors.white,
-              ),
-            ),
-            const SizedBox(height:65),
-
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Email',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(10),
-              placeholder: "email@email.com",
-              placeholderStyle: TextStyle(color:Colors.white70, fontSize:14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(7),
-                ),
-              ),
-            ),
-            const SizedBox(height:8),
-
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Senha',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const CupertinoTextField(
-              padding: EdgeInsets.all(10),
-              placeholder: "********",
-              placeholderStyle: TextStyle(color:Colors.white70, fontSize:14),
-              style: TextStyle(color: Colors.white, fontSize: 14),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(7),
-                ),
-              ),
-            ),
-            const SizedBox(height:30),
-            
             SizedBox(
               width: double.infinity,
               child: CupertinoButton(
